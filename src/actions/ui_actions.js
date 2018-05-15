568e9fdb742df59771c2677856677865678092ea
Kyle Burney <Kyle.Burney@nordstrom.com>
2018-05-15 16:29:18 -0700

    simplified action function

--- src/actions/ui_actions.js
+++ src/actions/ui_actions.js
@@ -2,9 +2,6 @@ import * as allActions from './action_types';
 
 export function loadMainPanelComponent(component) {
     return (dispatch) => {
-        dispatch(receiveMainPanelComponent(component))
+        dispatch({type: allActions.LOAD_MAIN_PANEL_COMPONENT, data: component})
     };
-}
-function receiveMainPanelComponent(data) {
-    return {type: allActions.LOAD_MAIN_PANEL_COMPONENT, data: data};
 }
